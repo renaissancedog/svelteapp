d9446145caa39c21bde59f78ab6c9308cacfd36c
William Liu <orangetnt827@gmail.com>
2024-09-25 21:12:21 -0500

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	let name = 'Svelte';
+</script>
+<h1 class="bg-green-300 border-green-600 border-b p-4 m-4 rounded hover:bg-blue-600">Hello {name.toUpperCase()}!</h1>
+<p class="text-center">Made with Svelte</p>
