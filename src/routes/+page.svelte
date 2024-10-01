ef2766080148191201275abe7fbd9686968ed73e
William Liu <orangetnt827@gmail.com>
2024-09-30 21:01:21 -0500

    add second page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
-<script>
-	let name = 'Svelte';
+<script lang="ts">
+    import { base } from '$app/paths';
+		let name="Svelte"
 </script>
+<a href="{base}/cs">cs</a>
 <h1 class="bg-green-300 border-green-600 border-b p-4 m-4 rounded hover:bg-blue-600">Hello {name.toUpperCase()}!</h1>
 <p class="text-center">Made with Svelte and Tailwind CSS</p>
---
+++ src/routes/cs/+page.svelte
@@ -0,0 +1,2 @@
+<h1 class="bg-red-300">Comp</h1>
+<p class="text-center">Made with Svelte and Tailwind CSS</p>
